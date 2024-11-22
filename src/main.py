639c1fe71a9e30160f250dba0d30946fc5f7fe19
Miron-Anosov <mairon26rus@gmail.com>
2024-11-22 18:02:14 +0100

    feat: create main function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Main module."""
+import logging
+import sys
+
+from src.console_core.console import ConsoleRunner
+from src.console_core.utils.front import make_front_console
+
+logger = logging.getLogger(__name__)
 
 
-def print_hello_user() -> None:
-    """Stdout massage."""
-    print("Hello User!")
+def main():
+    """Entrypoint."""
+    try:
+        utility = ConsoleRunner(console=make_front_console())
+        utility.run()
+    except KeyboardInterrupt:
+        logger.debug("main menu exit code: 1")
+        sys.exit(1)
+    except Exception as e:
+        logger.exception("main menu exception:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
