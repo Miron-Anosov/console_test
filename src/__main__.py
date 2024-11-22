d6d13db7e092ae9aa5fff6a04eb6e12f53ab2ae8
Miron-Anosov <mairon26rus@gmail.com>
2024-11-22 18:08:36 +0100

    feat: create entrypoint

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Program entry point."""
+
+from src.main import main
+
+
+def run() -> None:
+    """Run the main function."""
+    main()
+
+
+if __name__ == "__main__":
+    # Run with: `bash python -m src`
+    run()
